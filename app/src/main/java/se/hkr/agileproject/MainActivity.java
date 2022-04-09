a8a8a053d299bbdddf6621a5fd09bf4047d15e41
Hans <frittblas@gmail.com>
2022-04-09 23:30:16 +0200

    log in works

--- app/src/main/java/se/hkr/agileproject/MainActivity.java
+++ app/src/main/java/se/hkr/agileproject/MainActivity.java
@@ -2,20 +2,15 @@ package se.hkr.agileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.android.volley.*;
+import com.android.volley.toolbox.*;
 
-import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +36,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    void tryLogin(String user_name, String password) {
+    public void switchActivity(String name) {
+
+        Intent myIntent = new Intent(this, StartScreenActivity.class);
+        myIntent.putExtra("user_name", name);
+        startActivity(myIntent);
+
+    }
+
+    public void tryLogin(String user_name, String password) {
 
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://frittblas.se/agileproject/login.php" +
@@ -56,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
                         if(response.compareTo(user_name) == 0) {
                             // show a short little success msg (toast)
                             Toast.makeText(getApplicationContext(), "Logged in " + response, Toast.LENGTH_SHORT).show();
-                            // now we can safely switch activity to the start screen
+                            // switch activity to the start screen
+                            switchActivity(user_name);
                         }
                         else {
                             // show a short little error msg
--- app/src/main/java/se/hkr/agileproject/StartScreenActivity.java
+++ app/src/main/java/se/hkr/agileproject/StartScreenActivity.java
@@ -2,7 +2,9 @@ package se.hkr.agileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StartScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class StartScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        Intent myIntent = getIntent();
+        String user_name = myIntent.getStringExtra("user_name");
+        ((TextView)findViewById(R.id.user_name)).setText(user_name);
+
     }
 }
