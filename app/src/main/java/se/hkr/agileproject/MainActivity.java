da2595880254ab18bfc7eb1c3fa00bb38d890d22
robin <robin.cobanov@hotmail.com>
2022-04-27 12:00:35 +0200

    Moved encrypt methods and created tests

--- app/src/main/java/se/hkr/agileproject/MainActivity.java
+++ app/src/main/java/se/hkr/agileproject/MainActivity.java
@@ -81,31 +81,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // Returns a string with 61 chars
-    public String generateSecurePassword(String password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(password.getBytes());
-            byte[] digest = md.digest();
-            StringBuilder hexString = new StringBuilder();
-
-            for (byte b : digest) {
-                hexString.append(Integer.toHexString(0xFF & b));
-            }
-            return hexString.toString();
-        }
-        catch (NoSuchAlgorithmException e) {
-            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
-        }
-    }
-
-    // Encrypt users input to see if it matches password saved in db
-    public boolean verifyUserPassword(String providedPassword, String storedPassword) {
-        boolean confirmed = false;
-        String newSecurePassword = generateSecurePassword(providedPassword);
-        if (newSecurePassword.equals(storedPassword))
-            confirmed = true;
-
-        return confirmed;
-    }
 }
