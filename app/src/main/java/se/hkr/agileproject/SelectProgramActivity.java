080b186b0043ff034ca44516a64355f55b9eb86a
fwett <fwetterlov@gmail.com>
2022-05-03 18:00:25 +0200

    not functional getPrograms added

--- app/src/main/java/se/hkr/agileproject/SelectProgramActivity.java
+++ app/src/main/java/se/hkr/agileproject/SelectProgramActivity.java
@@ -7,9 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -26,6 +24,7 @@ import java.util.List;
 public class SelectProgramActivity extends AppCompatActivity {
 
     String currentUser;
+    List<String> programList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +32,11 @@ public class SelectProgramActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_program);
 
         // test array before we collect data with responseListener
-        String[] programs = new String[] {"Beginner", "Push", "Pull", "Legs"};
-
+        // String[] programs = new String[]{"Beginner", "Push", "Pull", "Legs"};
+        // List<String> programList = new ArrayList<String>(Arrays.asList(programs));
+        getPrograms();
         ListView listView = (ListView) findViewById(R.id.listview);
-        List<String> programList = new ArrayList<String>(Arrays.asList(programs));
+
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, programList);
         listView.setAdapter(arrayAdapter);
@@ -48,8 +48,33 @@ public class SelectProgramActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "You choose: " + selectedItem, Toast.LENGTH_SHORT).show();
             }
         });
-        
+
         Intent myIntent = getIntent();
         currentUser = myIntent.getStringExtra("username");
+    }
+
+    public void getPrograms() {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://frittblas.se/agileproject/getprograms.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // check if the response is good
+                        if (response.equals("no programs")) {
+                            // show a short little error msg
+                            Toast.makeText(getApplicationContext(), "Error: " + response, Toast.LENGTH_SHORT).show();
+                        } else {
+                            programList = Arrays.asList(response.split(" "));
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
