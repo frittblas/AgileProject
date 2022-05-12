bfd314e6cd10f3e1930b619c8791ce5ad0f08e6c
Hans Stromquist <frittblas@gmail.com>
2022-05-12 19:02:21 +0200

    latest wall post @home

--- app/src/main/java/se/hkr/agileproject/HomeActivity.java
+++ app/src/main/java/se/hkr/agileproject/HomeActivity.java
@@ -6,10 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     String currentUser;
+
+    List<String> messageList = new ArrayList<>();
 
     enum toActivity {
         SELECT_PROGRAM,
@@ -27,6 +41,9 @@ public class HomeActivity extends AppCompatActivity {
         // Assigning username to greeting message
         TextView greetUser = (TextView) findViewById(R.id.textUsername);
         greetUser.setText(currentUser);
+
+        // set latest post
+        getLatestPostAsync();
 
     }
 
@@ -58,6 +75,33 @@ public class HomeActivity extends AppCompatActivity {
 
     public void onClickWall(View v) {
         switchActivity(toActivity.THE_WALL);
+    }
+
+    public void setLatestPost(String post) {
+
+        TextView latestPost = (TextView) findViewById(R.id.latestPostHome);
+        latestPost.setText(post);
+
+    }
+
+    public void getLatestPostAsync() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://frittblas.se/agileproject/getwallposts.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        messageList = Arrays.asList(response.split(";"));
+                        setLatestPost(messageList.get(1));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(stringRequest);
     }
 
 }
