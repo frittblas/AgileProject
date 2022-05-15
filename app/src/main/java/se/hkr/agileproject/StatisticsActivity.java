18ee99e4fa97455f870cf0920439776c2ff39ae1
Hans Stromquist <frittblas@gmail.com>
2022-05-15 13:20:09 +0200

    statistics

--- app/src/main/java/se/hkr/agileproject/StatisticsActivity.java
+++ app/src/main/java/se/hkr/agileproject/StatisticsActivity.java
@@ -5,17 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class StatisticsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        populateGraph();
+
     }
 
     public void onClickBack(View v) {
 
         finish();
+
+    }
+
+    void populateGraph(){
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
     }
 
